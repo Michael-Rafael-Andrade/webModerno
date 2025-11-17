0eb685ee585c7d0a29e4e0997f83f79a66574290
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-17 19:56:48 -0300

    Create desafio_area.js

---
+++ javaScript/desafio_area.js
@@ -0,0 +1,18 @@
+// area = PI * raio * raio 
+const PI = 3.141592;
+// let raio = 10;
+const raio = 10;
+// let area = area = PI * raio * raio;
+let area = PI * raio * raio;  // Observe só consegue resolver uma vez
+
+area = Math.PI * raio * raio;
+
+console.log("O resultado da area é: " + area + " m2.");
+console.log(Math.PI);
+
+// const area = PI * raio * raio;  // Observe só consegue resolver uma vez
+
+// area = Math.PI * raio * raio;
+
+// console.log("O resultado da area é: " + area + " m2.");
+// // console.log(Math.PI);
