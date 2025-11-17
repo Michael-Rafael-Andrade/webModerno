bc2b014dbbc705c962c38db7ba76185435016171
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-17 00:28:59 -0300

    Create dados.js

---
+++ javaScript/dados.js
@@ -0,0 +1,12 @@
+var nome = "Caneta";
+var quantidade = 10; // criando uma variável chamada quantidade e atribuindo o valor 10.
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(quantidade);
+console.log(preco); // JavaScript usa o padrão americano . para separar as casas decimais
+console.log(imposto); // JavaScript usa o padrão americano . para separar as casas decimais
+// console.log(preco + imposto); // JavaScript usa o padrão americano . para separar as casas decimais
+console.log(precoFinal); // JavaScript usa o padrão americano . para separar as casas decimais
