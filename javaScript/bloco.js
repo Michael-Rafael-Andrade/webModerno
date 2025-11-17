82073d1d107d6a45e4e0f6fc04c5fef11a4ab1b0
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-17 00:18:47 -0300

    desafio_bloco_codigo
    desafio_bloco_codigo

--- javaScript/bloco.js
+++ javaScript/bloco.js
@@ -1,8 +1,25 @@
 // Bloco de códigos é delimitado por { abrir e fechar chave }
 {
     console.log("bloco: 1");
-    console.log("bloco: 2");
-    console.log("bloco: 3");
-    console.log("bloco: 4");
+    console.log("bloco: 1");
+    console.log("bloco: 1");
+    console.log("bloco: 1");
 
 }
+
+{
+    console.log("bloco: 2");
+    console.log("bloco: 2");
+    console.log("bloco: 2");
+    console.log("bloco: 2");
+}
+
+{
+    console.log("bloco 1");
+    {
+        // Percebe que este console.log() vai ficar na mesma linha do primeiro deste bloco.
+        console.log("bloco 1 - bloco 2 dentro do 1");
+    }
+}
+
+// Os códigos de programação são organizados em pastas > arquivos > blocos > sentenças de código!
---
+++ javaScript/desafio_sentenca_bloco.js
@@ -0,0 +1,23 @@
+/* 
+Exibir no console os números impares entre 1 até 10 no bloco 1
+
+Exibir no console os números pares entre 1 até 10 no bloco 2
+*/
+
+// Bloco 1
+{
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
+
+// Bloco 2
+{
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
