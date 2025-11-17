69bfe8ea8c76a512fa47d183dc5a5d2f1893914b
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-16 23:36:24 -0300

    curso - praticando
    curso - praticando

---
+++ sentenca_2.js
@@ -0,0 +1,12 @@
+console.log("texto deve ser escrito entre aspas dupla ou simples''");
+console.log('Evite usar aspas simples, porque vai ter linguagem que só vai aceitar aspas dupla por padrão!');
+
+console.log("Passo #01");
+console.log("Passo #02");
+console.log("Passo #03");
+
+// Cria comentário de uma linha
+/* Cria comentário mais de uma linha
+comentário...
+comentário...
+*/
