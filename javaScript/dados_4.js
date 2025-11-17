4f21ba5f69b34befee2c6a3738c4af08f58553b9
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-17 19:48:10 -0300

    Create dados_4.js

---
+++ javaScript/dados_4.js
@@ -0,0 +1,20 @@
+// const a = 3; // Deve mudar a const
+const a = 3;
+
+// ...
+
+a = a + 10;
+
+// ...
+
+console.log(a);
+
+// let a = 3;
+// // ...
+
+// a = 10;
+// a = a + 10; // assim faz a atribuição do valor ao valor que já tinha 3.
+
+// // ...
+
+// console.log(a);
