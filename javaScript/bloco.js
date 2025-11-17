5771496ba3aa8746e833f61486211b15390c3f25
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-17 00:00:16 -0300

    Create bloco.js
    bloco de código

---
+++ javaScript/bloco.js
@@ -0,0 +1,8 @@
+// Bloco de códigos é delimitado por { abrir e fechar chave }
+{
+    console.log("bloco: 1");
+    console.log("bloco: 2");
+    console.log("bloco: 3");
+    console.log("bloco: 4");
+
+}
