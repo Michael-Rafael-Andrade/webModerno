e02c150472c2d7e26db81a4cdb1cb2d6497390e3
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-18 20:23:19 -0300

    Create algunsCuidados.js

---
+++ exercicios-js/fundamentos/algunsCuidados.js
@@ -0,0 +1,6 @@
+console.log( 7 / 0);
+console.log("10.2" / 2); // observe que o resultado é = 5.1
+console.log("Show!" * 2); // not a number
+console.log(0.1 + 0.7); // não da 0.8
+// console.log(10.toString) // Não da certo fazer em cima de uma literal o toString()
+console.log((10.345).toFixed(2)); // Com o literal dentro de parenteses é possível chamar e usar o comando toFixed() para delimitar o número de casas.
