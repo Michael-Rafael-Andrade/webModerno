08898fd32c821bb8220ced55c4b3ad6f76f2e2ed
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-18 00:07:29 -0300

    Update organizacao.js

---
+++ exercicios-js/fundamentos/organizacao.js
@@ -0,0 +1,10 @@
+console.log("Sentença de código"); // representa uma sentança de código
+
+
+{
+    console.log("Olá! ");
+    { // Bloco agrupa sentenças de códigos
+        console.log("Sentenças Delimitado por bloco de código");
+
+    }
+}
