00d6211365922a5ddd5b766d9d89efa24b589f8d
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-18 20:00:18 -0300

    tipagemFraca
    tipagemFraca

---
+++ exercicios-js/fundamentos/variaveisEConstantes.js
@@ -0,0 +1,22 @@
+// Declarar variáveis
+var a = 3;
+// Atentar aos nomes que será dado a variável
+// Regra geral use let para criar variáveis
+
+let b = 4;  // let = palavra reservada
+
+var a = 30;  // Observe que a variável (var = a foi declarada duas vezes) e NÃO DEU ERRO! mas com Let sim, não permite declarar duas vezes.
+// let b = 40;
+b = 40;
+
+console.log(a, b);
+
+
+const c = 5;
+
+
+// c = 50;  // constante não permite alterar o seu valor.
+console.log(c);
+
+
+// console.log(a);
