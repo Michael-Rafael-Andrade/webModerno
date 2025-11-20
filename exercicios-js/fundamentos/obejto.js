e1c839b19e9f1f0bf2a4d1c2a7962f2af273bda4
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-20 10:36:38 -0300

    Update obejto.js

--- exercicios-js/fundamentos/obejto.js
+++ exercicios-js/fundamentos/obejto.js
@@ -1,1 +1,22 @@
 //json => JavaScript Object Notation. json formato textual  OBSERVAÇÃO
+const prod1 = {
+    casa: 'azul', //Posso inserir dados dentro de objeto desta forma
+};  // Par de chaves mostra que é um objeto
+prod1.nome = 'Celular Ultra Mega';
+prod1.preco = '4998.90';
+prod1['Desconto Legal'] = 0.40;  // evitar atributos com espaço
+ 
+console.log(prod1);
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+    obj: { // objeto dentro de outro objeto
+        blabla: 1, // objeto dentro de outro objeto
+        obj: {
+            blabla: 2,
+        }
+    }
+}
+
+console.log(prod2);
