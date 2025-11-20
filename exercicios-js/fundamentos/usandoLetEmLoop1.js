ebedfbe8fb96d6ab1ab15b702fde36aa58880b43
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2025-11-20 14:56:45 -0300

    Create usandoLetEmLoop1.js

---
+++ exercicios-js/fundamentos/usandoLetEmLoop1.js
@@ -0,0 +1,4 @@
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+console.log('i =', i); // variável i só está disponível dentro do escopo do bloco então apresentará um erro porque ela não será visível o let i .
